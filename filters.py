959fbeb6a3d0942f020036b58720faf5db606585
eromero-sorint <eromero@sorint.com>
2026-07-16 15:36:05 +0200

    Update filters.py

--- filters.py
+++ filters.py
@@ -1,1 +1,11 @@
+# filter_plugins/mis_filtros.py
 
+def decorar_texto(texto):
+    """Convierte el texto a mayúsculas y le añade estrellas."""
+    return f"✨ {texto.upper()} ✨"
+
+class FilterModule(object):
+    def filters(self):
+        return {
+            'decorar_texto': decorar_texto
+        }
