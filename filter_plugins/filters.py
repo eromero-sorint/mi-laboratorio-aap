bc407cdb41f2f956e268662b84d509195f4a92cf
eromero-sorint <eromero@sorint.com>
2026-07-23 11:47:07 +0200

    Update filters.py

--- filter_plugins/filters.py
+++ filter_plugins/filters.py
@@ -1,1 +1,8 @@
+def decorar_texto(texto):
+    return f"✨ {texto.upper()} ✨"
 
+class FilterModule(object):
+    def filters(self):
+        return {
+            'decorar_texto': decorar_texto
+        }
